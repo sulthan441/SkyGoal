90783965cbbd95d86ca9627c4b4977c59921ea93
sulthan441 <mohammedsulthan441@gmail.com>
2023-05-10 08:24:45 +0530

    server connected

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
 const express = require('express')
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT
+
+app.listen(PORT,()=>{
+    console.log(`server is running on the port ${PORT}`)
+})
